72294b8638d9938df56bc008eb6576ba5a428264
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-06-03 04:35:08 +0200

    add nav skeleton and search component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './assets/colored-logo.png';
+import './css-reset.css';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Nav from './components/navigation/Nav';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Clicable/Clicable.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ClicableElement = ({ child, click, keyDown }) => (
+  <button onClick={click} onKeyDown={keyDown} type="button">
+    {child}
+  </button>
+);
+
+export default ClicableElement;
+
+ClicableElement.propTypes = {
+  child: PropTypes.element.isRequired,
+  click: PropTypes.func,
+  keyDown: PropTypes.func,
+};
+
+ClicableElement.defaultProps = {
+  click: null,
+  keyDown: null,
+};
